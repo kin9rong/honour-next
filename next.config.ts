df7e5d7ecd147bb573126ccea31e99c2b395309a
jinrong.liu <jinrong.liu@email.ncu.edu.cn>
2025-06-15 03:36:52 +0800

    更新打包文件

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // 启用静态导出
+  distDir: 'dist',  // 将输出目录改为 dist
+  trailingSlash: true, // 为静态导出添加尾部斜杠
+  images: {
+    unoptimized: true, // 静态导出需要禁用图片优化
+  },
 };
 
 export default nextConfig;
